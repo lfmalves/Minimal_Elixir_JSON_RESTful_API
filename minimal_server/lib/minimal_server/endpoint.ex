3f8b308ff8336e6f3358ea7e1dcf4d3fbce11ca5
Luis F M Alves <luis.projensino@gmail.com>
2019-10-12 21:29:21 -0300

    feat: functions child_spec and start_link in endpoint.ex

--- minimal_server/lib/minimal_server/endpoint.ex
+++ minimal_server/lib/minimal_server/endpoint.ex
@@ -1,6 +1,8 @@
 defmodule MinimalServer.Endpoint do 
 	use Plug.Router
 
+  forward("/bot", to: MinimalServer.Router)
+
 	plug(:match)
 
 	plug(Plug.Parsers,
@@ -13,5 +15,16 @@ defmodule MinimalServer.Endpoint do
 
 	match _ do 
 		send_resp(conn, 404, "Requested page not found!")
+	end 
+
+	def child_spec(opts) do 
+		%{
+			id: __MODULE__,
+			start: {__MODULE__, :start_link, [opts]}
+		}
+	end 
+
+	def start_link(_opts) do 
+		Plug.Adapters.Cowboy2.http(__MODULE__, [])
 	end 
 end 
